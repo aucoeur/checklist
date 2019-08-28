cc1c4b6cc28d91c8d44bcf4b57d1a16ac2c6a192
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-08-28 16:02:25 -0700

    Create checklist github repo

---
+++ checklist.py
@@ -0,0 +1,33 @@
+# Create our Checklist
+checklist = list()
+
+# Define Functions
+# CREATE
+def create(item):
+    checklist.append(item)
+
+# READ
+def read(index):
+    return checklist[index]
+
+# UPDATE
+def update(index, item):
+    checklist[index] = item
+
+# DESTROY
+def destroy(index):
+    checklist.pop(index)
+
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(str(index) + list_item)
+        index += 1
+
+# TEST
+def test():
+  
+
+test()
+
+list_all_items()
