aab0cfb8232b03d5941ae68823c0b4095f1bc86b
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-08-28 16:07:42 -0700

    Complete page 1-3 of Rainbow Checklist tutorial

--- checklist.py
+++ checklist.py
@@ -18,16 +18,24 @@ def update(index, item):
 def destroy(index):
     checklist.pop(index)
 
-def list_all_items():
-    index = 0
-    for list_item in checklist:
-        print(str(index) + list_item)
-        index += 1
+# def list_all_items():
+#     index = 0
+#     for list_item in checklist:
+#         print(str(index) + list_item)
+#         index += 1
 
 # TEST
 def test():
-  
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+    destroy(1)
+
+    print(read(0))
+    print(read(1)) 
 
 test()
-
-list_all_items()
